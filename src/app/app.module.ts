1915a469ff7ccac22ef4ac7322350fa6782dc02a
Ana Carolina Porto <carolinamatosinhos98@gmail.com>
2023-05-09 09:40:01 -0300

    usuarios da api adicionados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -5,6 +6,7 @@ import { AppComponent } from './app.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { FormsModule } from '@angular/forms';
 import { UsersComponent } from './users/users.component';
+import { UsersService } from './users.service';
 
 @NgModule({
   declarations: [
@@ -14,9 +16,10 @@ import { UsersComponent } from './users/users.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [UsersService, HttpClientModule], //provider = fornecedor. São os serviços fornecedores
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,3 +1,4 @@
+import { UsersService } from './../users.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -5,14 +6,22 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.scss']
 })
-export class UsersComponent implements OnInit{
-  users: Array<any> = new Array();
+export class UsersComponent implements OnInit {
+  users: Array<any> = new Array(); //declarando a variavel users. A variavel começa sem nenhum valor. O código também inicializa a variável com um novo array vazio usando o construtor "new Array()".
 
-  ngOnInit(): void {
+  constructor(private userService: UsersService ) {
+
+  }
+
+  ngOnInit() {
     this.listUsers()
   }
 
   listUsers() {
-
+    this.userService.listUsers().subscribe(users => {
+      this.users = users;
+    }, err => {
+      console.log("Erro ao carregar usuários.")
+    } )
   }
 }
